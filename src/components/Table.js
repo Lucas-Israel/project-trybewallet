958fef797e149a622bb2b21fb4de5017446dd4e9
Lucas Israel <lisraelalves92@gmail.com>
2022-08-28 19:32:11 -0300

    7 feito

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Table extends Component {
   render() {
+    const { expenses } = this.props;
+    const abc = expenses
+      .map(({ currency, description, method, tag, value, exchangeRates }) => exchangeRates);
+    console.log(abc);
     return (
       <table style={ { width: '100%' } }>
         <thead>
@@ -13,13 +19,45 @@ class Table extends Component {
             <th>Tag</th>
             <th>Câmbio utilizado</th>
             <th>Valor convertido</th>
+            <th>Total convertido</th>
             <th>Moeda de conversão</th>
             <th>Editar/Excluir</th>
           </tr>
         </thead>
+        <tbody>
+          { expenses.length > 0 && expenses
+            .map(({ id, currency, description, method, tag, value, exchangeRates }) => (
+              <tr key={ id }>
+                <td>{(+value).toFixed(2)}</td>
+                <td>{description}</td>
+                <td>{exchangeRates[currency].name}</td>
+                <td>{method}</td>
+                <td>{tag}</td>
+                <td>{exchangeRates[currency].name}</td>
+                <td>{(+exchangeRates[currency].ask).toFixed(2)}</td>
+                <td>{((+value) * (+exchangeRates[currency].ask)).toFixed(2)}</td>
+                <td>Real</td>
+                <td><button type="button">del</button></td>
+              </tr>
+            )) }
+        </tbody>
       </table>
     );
   }
 }
 
-export default Table;
+Table.defaultProps = {
+  expenses: [],
+};
+
+Table.propTypes = {
+  expenses: PropTypes.arrayOf(
+    PropTypes.shape({}),
+  ),
+};
+
+const mapStateToProps = ({ wallet: { expenses } }) => ({
+  expenses,
+});
+
+export default connect(mapStateToProps)(Table);
