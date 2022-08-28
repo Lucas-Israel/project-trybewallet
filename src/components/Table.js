57d1a88d4ec1626f11f77657fd9f27b098af1e17
Lucas Israel <lisraelalves92@gmail.com>
2022-08-28 18:24:18 -0300

    6 feito

--- src/components/Table.js
+++ src/components/Table.js
@@ -3,7 +3,21 @@ import React, { Component } from 'react';
 class Table extends Component {
   render() {
     return (
-      <div>Table</div>
+      <table style={ { width: '100%' } }>
+        <thead>
+          <tr>
+            <th>Valor</th>
+            <th>Descrição</th>
+            <th>Moeda</th>
+            <th>Método de pagamento</th>
+            <th>Tag</th>
+            <th>Câmbio utilizado</th>
+            <th>Valor convertido</th>
+            <th>Moeda de conversão</th>
+            <th>Editar/Excluir</th>
+          </tr>
+        </thead>
+      </table>
     );
   }
 }
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -2,14 +2,16 @@ import React from 'react';
 import { connect } from 'react-redux';
 import Header from '../components/Header';
 import WalletForm from '../components/WalletForm';
+import Table from '../components/Table';
 
 class Wallet extends React.Component {
   render() {
     return (
       <>
         <Header />
-        <br />
         <WalletForm />
+        <br />
+        <Table />
       </>
     );
   }
